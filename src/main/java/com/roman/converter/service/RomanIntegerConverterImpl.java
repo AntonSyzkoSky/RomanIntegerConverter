0933046bb1d5f1bfaa30154a14c50343dc4bb3a6
Anton Syzko <anton.syzko@sky.uk>
2022-08-29 21:06:26 +0100

    Implement int to roman conversion

--- src/main/java/com/roman/converter/service/RomanIntegerConverterImpl.java
+++ src/main/java/com/roman/converter/service/RomanIntegerConverterImpl.java
@@ -1,10 +1,25 @@
 package com.roman.converter.service;
 
+import java.util.Map;
+
 public class RomanIntegerConverterImpl implements RomanIntegerConverter {
+
+    public final  int[] VALUES_FOR_INT_CONVERSION = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    public final  String[] ROMAN_NUMBERS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     @Override
     public String convertIntegerToRoman(Integer number) {
-        return null;
+
+
+        StringBuilder romanNumber = new StringBuilder();
+
+        for (int i = 0; number > 0; i++)
+            while (number >= VALUES_FOR_INT_CONVERSION[i]) {
+                romanNumber.append(ROMAN_NUMBERS[i]);
+                number -= VALUES_FOR_INT_CONVERSION[i];
+            }
+
+        return romanNumber.toString();
     }
 
     @Override
