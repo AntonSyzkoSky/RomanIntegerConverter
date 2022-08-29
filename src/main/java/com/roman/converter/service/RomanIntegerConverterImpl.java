e46194754c5955acd40639416de5bd533a5ef5dd
Anton Syzko <anton.syzko@sky.uk>
2022-08-29 21:07:13 +0100

    Implement roman to int conversion

--- src/main/java/com/roman/converter/service/RomanIntegerConverterImpl.java
+++ src/main/java/com/roman/converter/service/RomanIntegerConverterImpl.java
@@ -7,6 +7,16 @@ public class RomanIntegerConverterImpl implements RomanIntegerConverter {
     public final  int[] VALUES_FOR_INT_CONVERSION = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     public final  String[] ROMAN_NUMBERS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
+    public final  Map<Character, Integer> ROMAN_TO_INT_DICTIONARY = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000
+    );
+
     @Override
     public String convertIntegerToRoman(Integer number) {
 
@@ -24,6 +34,22 @@ public class RomanIntegerConverterImpl implements RomanIntegerConverter {
 
     @Override
     public Integer convertRomanToInteger(String romanNumber) {
-        return null;
+        Integer intResult = 0;
+
+        for (int i = 0; i < romanNumber.length(); i++) {
+
+            if(!ROMAN_TO_INT_DICTIONARY.containsKey(romanNumber.charAt(i))){
+                throw new NumberFormatException("Latin letter provided is not used in roman math");
+            }
+            //exceptions of IV , IX etc ...
+            if (i > 0 && ROMAN_TO_INT_DICTIONARY.get(romanNumber.charAt(i)) > ROMAN_TO_INT_DICTIONARY.get(romanNumber.charAt(i - 1))) {
+
+                intResult += ROMAN_TO_INT_DICTIONARY.get(romanNumber.charAt(i)) - 2 * ROMAN_TO_INT_DICTIONARY.get(romanNumber.charAt(i - 1));
+
+            } else {
+                intResult += ROMAN_TO_INT_DICTIONARY.get(romanNumber.charAt(i));
+            }
+        }
+        return intResult;
     }
 }
