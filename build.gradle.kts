c227980eb83e71838b1dc2cd4cd7794c78b2de6f
Anton Syzko <anton.syzko@sky.uk>
2022-08-29 20:38:12 +0100

    Set up gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,23 @@ plugins {
     java
 }
 
-group = "org.example"
+group = "com.roman.test"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.0"))
+    testImplementation(platform("io.cucumber:cucumber-bom:7.6.0"))
+
+    testImplementation("io.cucumber:cucumber-java")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine")
+    testImplementation("org.junit.platform:junit-platform-suite")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("org.assertj:assertj-core:3.21.0")
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+repositories {
+    mavenLocal()
+    mavenCentral()
 }
 
 tasks.getByName<Test>("test") {
